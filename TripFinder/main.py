d6367420058c989afca67abf59408f1b454c781f
MeherDeepak-2005 <68017128+MeherDeepak-2005@users.noreply.github.com>
2020-08-16 12:38:41 +0530

    Add files via upload

---
+++ TripFinder/main.py
@@ -0,0 +1,32 @@
+from flask import *
+from datetime import timedelta
+
+app = Flask(__name__)
+
+app.secret_key = "H8QEt7JrraIV"
+app.permanent_session_lifetime = timedelta(days=14)
+
+
+
+
+@app.route('/Home_Page', methods=["POST","GET"])
+def Home_Page():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["usr_name"]
+        session['user'] = user
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("index.html")
+
+@app.route('/<usr>')
+def user(usr):
+    if "user" in session:
+        user = session["user"]
+        return render_template("main_page.html")
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
